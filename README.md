32272074ff4e8c34595d123ca081735e1b1fcac6
Tomos <tomoswyn@msn.com>
2013-09-04 10:35:08 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Show transcript
+
+A simple plugin that adds in a `[transcript] ... transcript content here ... [/transcript]`
+shortcode to your WordPress install. By default the content is hidden, and a link is show, and
+when clicked the transcript will slide out.
---
+++ class-show-transcript.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* Show Transcript Plugin
+*
+* A simple plugin based off the plugin boilerplate to add a transcript shortcode to a post/page.
+*
+* @package show-transcript
+* @author Base93 <tomos@base93.com>
+* @license GPL-2.0+
+* @link http://base93.com
+* @copyright 2013 Base93
+*
+* @wordpress-plugin
+* Plugin Name: Show / Hide Transcript
+* Plugin URI: https://github.com/base93/Show-Hide-Transcript
+* Description: A simple plugin that adds in a transcript shortcode for transcripts.
+* Version: 0.1
+* Author: Base93
+* Author URI: http://base93.com
+* Text Domain: show-transcript
+* License: GPL-2.0+
+* License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+* Domain Path: /lang
+*/
